1a9c83e9b12403ee41c2e4918e2a76e22d9b499f
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-10-28 17:35:22 +0530

    Code 145 Day 45

---
+++ Pattern67.java
@@ -0,0 +1,18 @@
+package Patterns3;
+public class Pattern67 {
+	public static void main(String[] args) {
+		int n = 3;
+					
+		for(int i=n; i >= -n; i--) // i=3,2,1,0,-1,-2,-3 ->7 rows
+			{
+				for(int j= Math.abs(i); j <= n ; j++)
+				//j=3, 3 >= 3 -> t // 1 row
+			    // j=2, 2>= 3 -> f
+					
+				//j=3, 3 >= 3 -> t  // 2 row
+				//j=2, 2 >= 3 -> t
+				System.out.print((char)(j+65)+" ");  // *
+						
+				System.out.println();					
+			}
+}}
